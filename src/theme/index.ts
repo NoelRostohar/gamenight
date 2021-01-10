5f80c8f80d7dd6fe122cae5cddcc11d3ea70fb6a
Noel Rostohar <noel.rostohar@gmail.com>
2021-01-10 22:38:44 +0100

    Added App Color Theme

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+import { Theme } from 'src/types'
+
+const theme: Theme = {
+    dark: '#20232A',
+    darkAccent: '#3C3E4A',
+    primary: '#03AF45',
+    light: '#fff',
+    faded: '#BABABA',
+    lightFade: '#DADADA',
+    confirmation: '#256FFF',
+    danger: '#FF0000'
+}
+
+export default theme;
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Theme {
+    dark: string,
+    darkAccent: string,
+    primary: string,
+    light: string,
+    faded: string,
+    lightFade: string,
+    confirmation: string,
+    danger: string
+}
