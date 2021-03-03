f446854c6513ea14c4964637b3546c565a79c349
NoelRostohar <noel.rostohar@arsfutura.co>
2021-03-03 15:21:47 +0100

    ParticipantCard Change Button Design

--- src/components/ParticipantCard.tsx
+++ src/components/ParticipantCard.tsx
@@ -1,80 +1,113 @@
-import React, { useCallback } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useCallback } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import Divider from './Divider';
+import Divider from "./Divider";
 
-import theme from '../theme';
-import { GameType, Participant } from 'src/types';
+import theme from "../theme";
+import { GameType, Participant } from "src/types";
 
 interface ParticipantCard {
-  participant: Participant;
+	participant: Participant;
+	isUser: boolean;
 }
 
-const ParticipantCard: React.FC<ParticipantCard> = ({ participant }) => {
-  const gamelist = useCallback(
-    (games: GameType[]) => {
-      const MAX_LENGTH: number = 3;
+const ParticipantCard: React.FC<ParticipantCard> = ({
+	participant,
+	isUser,
+}) => {
+	const gamelist = useCallback(
+		(games: GameType[]) => {
+			const MAX_LENGTH: number = 3;
 
-      if (games.length === 1) return games[0].name;
-      else if (games.length > MAX_LENGTH)
-        return games
-          .slice(0, MAX_LENGTH)
-          .reduce(
-            (prevValue, currValue, index, array) =>
-              prevValue +
-              (index < 1
-                ? ''
-                : index < array.length
-                ? ', '
-                : ` and ${games.length - MAX_LENGTH} more...`) +
-              currValue.name,
-            ''
-          );
+			if (games.length === 1) return games[0].name;
+			else if (games.length > MAX_LENGTH)
+				return games
+					.slice(0, MAX_LENGTH)
+					.reduce(
+						(prevValue, currValue, index, array) =>
+							prevValue +
+							(index < 1
+								? ""
+								: index < array.length
+								? ", "
+								: ` and ${games.length - MAX_LENGTH} more...`) +
+							currValue.name,
+						""
+					);
 
-      return games.reduce(
-        (prevValue, currValue, index, array) =>
-          prevValue +
-          (index < 1 ? '' : index < array.length - 1 ? ', ' : ' and ') +
-          currValue.name,
-        ''
-      );
-    },
-    [participant.games]
-  );
+			return games.reduce(
+				(prevValue, currValue, index, array) =>
+					prevValue +
+					(index < 1 ? "" : index < array.length - 1 ? ", " : " and ") +
+					currValue.name,
+				""
+			);
+		},
+		[participant.games]
+	);
 
-  return (
-    <>
-      <View style={styles.root}>
-        <MaterialIcons
-          name="account-circle"
-          size={36}
-          color={theme.darkAccent}
-        />
-        <View style={{ marginLeft: 5 }}>
-          <Text style={styles.username}>{participant.username}</Text>
-          <Text style={styles.games}>{gamelist(participant.games)}</Text>
-        </View>
-      </View>
-      <Divider />
-    </>
-  );
+	return (
+		<>
+			<View style={styles.root}>
+				<View style={styles.userInfo}>
+					<MaterialIcons
+						name='account-circle'
+						size={36}
+						color={theme.darkAccent}
+					/>
+					<View style={styles.container}>
+						<Text style={styles.username}>{participant.username}</Text>
+						<Text style={styles.games}>{gamelist(participant.games)}</Text>
+					</View>
+				</View>
+				{isUser && (
+					<TouchableOpacity style={styles.button}>
+						<Text style={styles.buttonText}>Change</Text>
+					</TouchableOpacity>
+				)}
+			</View>
+			<Divider />
+		</>
+	);
 };
 
 const styles = StyleSheet.create({
-  root: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    marginVertical: 15,
-  },
-  username: {
-    fontSize: 16,
-    color: theme.light,
-  },
-  games: {
-    fontSize: 14,
-    color: theme.faded,
-  },
+	root: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginVertical: 15,
+		justifyContent: "space-between",
+	},
+	userInfo: {
+		flexDirection: "row",
+		flex: 3,
+		flexWrap: "wrap",
+	},
+	container: {
+		marginLeft: 5,
+		flex: 3,
+	},
+	username: {
+		fontSize: 16,
+		color: theme.light,
+		flexShrink: 1,
+	},
+	games: {
+		fontSize: 14,
+		color: theme.faded,
+	},
+	button: {
+		flex: 1,
+		borderRadius: 150,
+		backgroundColor: theme.primary,
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 5,
+	},
+	buttonText: {
+		color: theme.light,
+	},
 });
 
 export default ParticipantCard;
--- src/screens/GameNight/Overview.tsx
+++ src/screens/GameNight/Overview.tsx
@@ -1,79 +1,87 @@
-import React, { memo } from 'react';
-import { View, ScrollView, Text, StyleSheet } from 'react-native';
-import { format } from 'date-fns';
+import React, { memo } from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import { format } from "date-fns";
+import { useSelector } from "react-redux";
 
-import IconText from '../../components/IconText';
-import ParticipantCard from '../../components/ParticipantCard';
+import IconText from "../../components/IconText";
+import ParticipantCard from "../../components/ParticipantCard";
 
-import theme from '../../theme';
-import { GamenightType, Participant } from '../../types';
+import theme from "../../theme";
+import { GamenightType, Participant } from "../../types";
+import { GlobalState } from "../../store";
 
 interface OverviewProps {
-  gamenight: GamenightType | undefined;
+	gamenight: GamenightType | undefined;
 }
 
 const Overview: React.FC<OverviewProps> = ({ gamenight }) => {
-  if (!gamenight) return null;
+	if (!gamenight) return null;
 
-  const { date, participants, place, time } = gamenight;
+	const user = useSelector((state: GlobalState) => state.user);
 
-  return (
-    <ScrollView contentContainerStyle={styles.bg}>
-      <View style={styles.iconContainer}>
-        <IconText
-          icon="calendar-today"
-          size={16}
-          text={format(new Date(date), 'dd/MM')}
-          color={theme.light}
-        />
-      </View>
-      <View style={styles.iconContainer}>
-        <IconText
-          icon="timer"
-          size={16}
-          text={format(new Date(time), 'kk:mm')}
-          color={theme.light}
-        />
-      </View>
-      <View style={styles.iconContainer}>
-        <IconText
-          icon="location-pin"
-          size={16}
-          text={place ? place.name : 'To be determined'}
-          color={theme.light}
-          subtext={place && place.address}
-        />
-      </View>
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Participants</Text>
-      </View>
-      {participants.map((participant: Participant) => {
-        return (
-          <ParticipantCard key={participant.id} participant={participant} />
-        );
-      })}
-    </ScrollView>
-  );
+	const { date, participants, place, time } = gamenight;
+
+	return (
+		<ScrollView contentContainerStyle={styles.bg}>
+			<View style={styles.iconContainer}>
+				<IconText
+					icon='calendar-today'
+					size={16}
+					text={format(new Date(date), "dd/MM")}
+					color={theme.light}
+				/>
+			</View>
+			<View style={styles.iconContainer}>
+				<IconText
+					icon='timer'
+					size={16}
+					text={format(new Date(time), "kk:mm")}
+					color={theme.light}
+				/>
+			</View>
+			<View style={styles.iconContainer}>
+				<IconText
+					icon='location-pin'
+					size={16}
+					text={place ? place.name : "To be determined"}
+					color={theme.light}
+					subtext={place && place.address}
+				/>
+			</View>
+			<View style={styles.section}>
+				<Text style={styles.sectionTitle}>Participants</Text>
+			</View>
+			{participants.map((participant: Participant) => {
+				return (
+					<ParticipantCard
+						key={participant.id}
+						participant={participant}
+						isUser={participant.username === user}
+					/>
+				);
+			})}
+		</ScrollView>
+	);
 };
 
 const styles = StyleSheet.create({
-  bg: {
-    paddingHorizontal: 20,
-    paddingBottom: 20,
-    paddingTop: 10,
-  },
-  iconContainer: {
-    marginVertical: 10,
-  },
-  section: {
-    paddingTop: 20,
-    paddingBottom: 10,
-  },
-  sectionTitle: {
-    color: theme.faded,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+	bg: {
+		paddingHorizontal: 20,
+		paddingBottom: 20,
+		paddingTop: 10,
+	},
+	iconContainer: {
+		marginVertical: 10,
+	},
+	section: {
+		paddingTop: 20,
+		paddingBottom: 10,
+	},
+	sectionTitle: {
+		color: theme.faded,
+		fontSize: 16,
+		fontWeight: "bold",
+	},
 });
 
 export default memo(Overview);
