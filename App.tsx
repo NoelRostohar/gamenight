4c5214be289cbd626c4d549899b08f7d3d8eabe4
NoelRostohar <noel.rostohar@arsfutura.co>
2021-03-04 12:15:53 +0100

    Added Socket IO, Connecting Via Websockets When Chat Component Mounts & Disconnectin On Unmount

--- App.tsx
+++ App.tsx
@@ -1,46 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 
 import {
-  NavigationContainer,
-  DefaultTheme,
-  Theme,
-} from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+	NavigationContainer,
+	DefaultTheme,
+	Theme,
+} from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import GetInitialData from './src/components/GetInitialData';
-import Splash from './src/screens/Splash';
+import GetInitialData from "./src/components/GetInitialData";
+import Splash from "./src/screens/Splash";
 
-import theme from './src/theme';
-import reducers from './src/store';
+import theme from "./src/theme";
+import reducers from "./src/store";
 
 const navTheme: Theme = {
-  dark: true,
-  colors: {
-    ...DefaultTheme.colors,
-    background: theme.dark,
-    text: theme.light,
-    card: theme.dark,
-    primary: theme.primary,
-  },
+	dark: true,
+	colors: {
+		...DefaultTheme.colors,
+		background: theme.dark,
+		text: theme.light,
+		card: theme.dark,
+		primary: theme.primary,
+	},
 };
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 const App = () => {
-  const [isAutheniticated, setIsAuthenticated] = useState<boolean>(false);
-
-  return (
-    <Provider store={store}>
-      <NavigationContainer theme={navTheme}>
-        <GetInitialData />
-        <StatusBar style="inverted" />
-        <Splash />
-      </NavigationContainer>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<NavigationContainer theme={navTheme}>
+				<GetInitialData />
+				<StatusBar style='inverted' />
+				<Splash />
+			</NavigationContainer>
+		</Provider>
+	);
 };
 
 export default App;
--- src/screens/GameNight/Chat.tsx
+++ src/screens/GameNight/Chat.tsx
@@ -1,75 +1,87 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	FlatList,
+} from "react-native";
+import { io } from "socket.io-client";
 
-import Input from '../../components/Input';
-import ChatBubble from '../../components/ChatBubble';
+import Input from "../../components/Input";
+import ChatBubble from "../../components/ChatBubble";
 
-import theme from '../../theme';
-import { GamenightType } from '../../types';
+import theme from "../../theme";
+import { GamenightType } from "../../types";
 
 interface ChatProps {
-  gamenight: GamenightType | undefined;
+	gamenight: GamenightType | undefined;
 }
 
 const Chat: React.FC<ChatProps> = ({ gamenight }) => {
-  if (!gamenight) return null;
+	if (!gamenight) return null;
 
-  const { chat } = gamenight;
-  return (
-    <>
-      {chat.length > 0 ? (
-        <FlatList
-          data={chat}
-          renderItem={({ item }) => <ChatBubble chat={item} />}
-          keyExtractor={({ id }) => id.toString()}
-          contentContainerStyle={{ paddingHorizontal: 20 }}
-          inverted
-        />
-      ) : (
-        <View style={styles.noChatContainer}>
-          <Text style={styles.noChatText}>No messages yet.</Text>
-        </View>
-      )}
-      <View style={styles.inputContainer}>
-        <Input
-          icon="send"
-          flexSize={1}
-          placeholder="Send a message"
-          button={
-            <TouchableOpacity>
-              <Text style={styles.button}>SEND</Text>
-            </TouchableOpacity>
-          }
-        />
-      </View>
-    </>
-  );
+	const { chat } = gamenight;
+
+	useEffect(() => {
+		const socket = io("http://192.168.34.87:3000");
+
+		socket.connect();
+
+		return () => {
+			socket.disconnect();
+		};
+	}, []);
+
+	return (
+		<>
+			{chat.length > 0 ? (
+				<FlatList
+					data={chat}
+					renderItem={({ item }) => <ChatBubble chat={item} />}
+					keyExtractor={({ id }) => id.toString()}
+					contentContainerStyle={{ paddingHorizontal: 20 }}
+					inverted
+				/>
+			) : (
+				<View style={styles.noChatContainer}>
+					<Text style={styles.noChatText}>No messages yet.</Text>
+				</View>
+			)}
+			<View style={styles.inputContainer}>
+				<Input
+					icon='send'
+					flexSize={1}
+					placeholder='Send a message'
+					button={
+						<TouchableOpacity>
+							<Text style={styles.button}>SEND</Text>
+						</TouchableOpacity>
+					}
+				/>
+			</View>
+		</>
+	);
 };
 
 const styles = StyleSheet.create({
-  noChatContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-  noChatText: {
-    color: theme.faded,
-  },
-  inputContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  button: {
-    color: theme.primary,
-  },
+	noChatContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		flex: 1,
+	},
+	noChatText: {
+		color: theme.faded,
+	},
+	inputContainer: {
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		alignItems: "center",
+		flexDirection: "row",
+	},
+	button: {
+		color: theme.primary,
+	},
 });
 
 export default memo(Chat);
